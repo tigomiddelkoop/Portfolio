23de171639ee85a6cfa5b82f0fce96f96f043810
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2020-12-31 14:34:13 +0100

    More stuff, i cannot remember precisly what i did

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
                     <link rel="icon" href="/favicon.ico"/>
                 </Head>
                 <main className={"h-full dark:text-white"}>
-                    <div className={"flex flex-col"}>
+                    <div className={"flex flex-col mb-4"}>
                         <div className="flex mb-4 items-center">
 
                             <div className="flex rounded-full shadow-lg">
@@ -34,12 +34,60 @@ export default function Home() {
                         </div>
                         <div className={"flex flex-col"}>
                             <p>Interested in my CV? Generate one!</p>
-                            <Button className={"lg:8w-32 text-center"} color={"success"}>Generate CV</Button>
+                            <Button className={"lg:w-32 text-center"} color={"success"}>Generate CV</Button>
                         </div>
                     </div>
                     <div>
                         <h1 className={"text-4xl"}>Tools and languages</h1>
-                        <h1 className={"text-1xl"}>Tools I can use (and I probably use on the regular)</h1>
+                        <h2 className={"text-1xl"}>Tools I can use (and I probably use on the regular)</h2>
+
+                        <div className={"flex justify-center mb-4"}>
+
+
+                            <div className={"w-64"}>
+                                <div className={"p-2 bg-red-600 h-32  border-l border-r border-t border-red-500 rounded-t-lg"}>
+                                    {/*<Image*/}
+                                    {/*    className={""}*/}
+                                    {/*    alt="Tigo"*/}
+                                    {/*    src={"/js-brands.svg"}*/}
+                                    {/*    height={64}*/}
+                                    {/*    width={64}*/}
+                                    {/*/>*/}
+                                    <h1 className={"font-bold ml-1 text-2xl text-white"}>Frameworks</h1>
+                                </div>
+                                <div
+                                    className={"bg-gray-50 border-gray-300 border-b border-l border-r rounded-b-lg"}>
+                                    <div className={"border-b border-gray-300 py-3 px-3"} >GraphQL</div>
+                                    <div className={"border-b border-gray-300 py-3 px-3"} >React</div>
+                                    <div className={"border-b border-gray-300 py-3 px-3"} >ExpressJS</div>
+                                    <div className={"py-3 px-3"} >NextJS</div>
+                                </div>
+                            </div>
+                            <div
+                                className={"w-64 rounded-lg"}>
+                                <h1 className={"text-center"}>Languages</h1>
+                                <div className={"border-gray-300 bg-gray-50 p-4 border"}>
+                                    <p>test</p>
+                                    <p>test</p>
+                                    <p>test</p>
+                                    <p>test</p>
+                                    <p>test</p>
+                                    <p>test</p>
+                                </div>
+                            </div>
+                            <div className={"w-64 rounded-lg"}>
+                                <h1 className={"text-center"}>Tools</h1>
+                                <div className={"border-gray-300 bg-gray-50 p-4 border "}>
+                                    <p>test</p>
+                                    <p>test</p>
+                                    <p>test</p>
+                                    <p>test</p>
+                                    <p>test</p>
+                                    <p>test</p>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
                 </main>
             </div>
