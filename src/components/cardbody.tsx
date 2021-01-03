48f43de988eb21ded5be8e737a76feb06ab908a7
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2021-01-03 02:05:23 +0100

    Fixed issue with color of cardbody

--- src/components/cardbody.tsx
+++ src/components/cardbody.tsx
@@ -1,6 +1,6 @@
 export default function CardBody({children}) {
     return (
-        <div className={"bg-gray-700 rounded-b-lg border-gray-300 dark:border-gray-600"}>
+        <div className={"dark:bg-gray-700 bg-gray-50 rounded-b-lg border-gray-300 dark:border-gray-600"}>
             {children}
         </div>
     )
