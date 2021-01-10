f93ea50e90515945f1a273f72ee0712bd08fd26f
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2021-01-11 00:14:32 +0100

    F*CK the key thingy, why did it take me so long to figure out...

--- src/components/home/skillscard.tsx
+++ src/components/home/skillscard.tsx
@@ -13,7 +13,7 @@ export default function Skillscard(props: props) {
     if (entries == undefined) entries = []
 
     return (
-        <div className={"rounded-lg w-full lg:w-96 mb-4 lg:mx-1"}>
+        <div key={props.name} className={"rounded-lg w-full lg:w-96 mb-4 lg:mx-1"}>
             <div style={{backgroundImage: `url("${props.image}")`}}
                  className={styles.background + " shadow-lg flex items-center justify-center p-2 h-32 border-l border-r border-t border-gray-300 border-b bg-gray-400 rounded-t-lg dark:bg-gray-900 dark:border-gray-600"}>
                 <h1 className={"font-bold ml-1 text-3xl text-white"}>{props.name}</h1>
@@ -48,22 +48,22 @@ function renderEntry(entry) {
 
     // LET IT RENDER WHOOOOOOOOOOOO
     return (
-        <div className={"flex border-gray-300 dark:border-gray-600 py-3 px-3 items-center"}>
+        <div key={"entry" + entry.name}  className={"flex border-gray-300 dark:border-gray-600 py-3 px-3 items-center"}>
             <div className={"flex-1"}>
                 {Array.isArray(entry.name) ? entry.name.map(name => {
-                    if (name == entry.name[0]) return <p className={"font-semibold"}>{name}</p>
+                    if (name == entry.name[0]) return <p key={name} className={"font-semibold"}>{name}</p>
                     return <p key={name} className={"text-xs font-light"}>{name}</p>
 
-                }) : <p  className={"font-semibold"}>{entry.name}</p>}
+                }) : <p key={entry.name} className={"font-semibold"}>{entry.name}</p>}
 
             </div>
             <div className={"text-right font-light"}>
-                <p className={entry.confidence !== undefined ? "text-xs" : "text-sm"}>
+                <p key={"experience" + entry.name} className={entry.confidence !== undefined ? "text-xs" : "text-sm"}>
                     {years !== "0" ? <span>{years} year{years !== '1' ? 's' : ''} </span> : ""}
                     {years !== "0" && months !== "0" ? " and " : ""}
                     {months !== "0" ? <span>{months} month{months !== '1' ? 's' : ''} </span> : ""}
                     experience</p>
-                {entry.confidence !== undefined ? <p className={"text-xs"}>Confidence: {confidence} ({entry.confidence}%)</p> : ""}
+                {entry.confidence !== undefined ? <p key={"confidence" + entry.name} className={"text-xs"}>Confidence: {confidence} ({entry.confidence}%)</p> : ""}
             </div>
         </div>
     )
