3be348e2c9dd6ec3d0bb49edd8b28f5a0dccef6a
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2021-01-15 22:36:50 +0100

    Added icons to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,6 +2,7 @@ import Pill from "./pill";
 import styles from "./footer.module.scss"
 import {useEffect, useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGithub, faLinkedin, faTwitter} from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
 
@@ -15,8 +16,8 @@ export default function Footer() {
         className={"align-bottom p-4 border-t border-b border-gray-100 items-center justify-center text-center dark:bg-gray-800 dark:border-gray-500 dark:text-white"}>
         <h1 className={"text-2xl"}>tigo.tech</h1>
         <div className={"flex flex-col lg:flex-row items-center justify-center text-center"}>
-            <div className={styles.footerCategoryContainer}>
-                <p className={styles.footerCategoryTitle}>Sites</p>
+            <div className={styles.categoryContainer}>
+                <p className={styles.categoryTitle}>Sites</p>
                 <div className={"flex items-center"}><a href={"https://tigo.tech"}
                                                         className={"mr-1"}>tigo.tech</a><Pill color={"info"}>You're
                     here</Pill></div>
@@ -25,19 +26,30 @@ export default function Footer() {
                 <div className={"flex items-center"}><a href={"https://systemmanager.io"}>SystemManager Core</a></div>
             </div>
 
-            <div className={styles.footerCategoryContainer}>
-                <p className={styles.footerCategoryTitle}>Socials</p>
-                <p className={styles.footerCategorySubTitle}>These socials I am willing to give</p>
-                <div><a rel={"noreferrer noopener _blank"} href={"https://github.com/tigomiddelkoop"}>GitHub</a></div>
-                <div><a rel={"noreferrer noopener _blank"}
-                        href={"https://www.linkedin.com/in/tigo-middelkoop-92067a15b/"}>LinkedIn</a></div>
-                {/*<div><a>Twitter</a></div>*/}
+            <div className={styles.categoryContainer}>
+                <div>
+                    <p className={styles.categoryTitle}>Socials</p>
+                </div>
+                <div className={styles.socialMediaCategory}>
+                    <a className={styles.socialMediaLink} rel={"noreferrer noopener _blank"}
+                       href={"https://github.com/tigomiddelkoop"}><FontAwesomeIcon width={16} className={"mr-1"}
+                                                                                   icon={faGithub}/> GitHub</a>
+
+                    <a className={"flex flex-row my-0.5"} rel={"noreferrer noopener _blank"}
+                       href={"https://www.linkedin.com/in/tigo-middelkoop-92067a15b/"}><FontAwesomeIcon width={16}
+                                                                                                        className={"mr-1"}
+                                                                                                        icon={faLinkedin}/> LinkedIn</a>
+                    <a className={"flex flex-row my-0.5"} rel={"noreferrer noopener _blank"}
+                       href={"https://twitter.com/__Tigo__"}><FontAwesomeIcon width={16}
+                                                                              className={"mr-1"}
+                                                                              icon={faTwitter}/> Twitter</a>
+                </div>
             </div>
 
-            <div className={styles.footerCategoryContainer}>
+            <div className={styles.categoryContainer}>
                 <div className={"mb-2"}>
-                    <p className={styles.footerCategoryTitle}>Kubernetes info</p>
-                    <div className={styles.footerCategorySubTitle}>
+                    <p className={styles.categoryTitle}>Kubernetes info</p>
+                    <div className={styles.categorySubTitle}>
                         <p className={"text-xs"}>Its not in kubernetes yet</p>
                         {/*<p className={"text-xs"}>That's right! It's deployed in kubernetes!</p>*/}
                         {/*<p className={"text-xs"}>I'm even learning kubernetes</p>*/}
@@ -56,9 +68,11 @@ export default function Footer() {
                 </div>
             </div>
         </div>
-        <p className={"text-xs font-light"}>All the pictures on this site are made by Tigo and are of the code of this site and the
+        <p className={"text-xs font-light"}>All the pictures on this site are made by Tigo and are of the code of this
+            site and the
             servers it is hosted on, the site might also contain pictures of projects he has done.</p>
-        <p className={"text-xs font-light"}>(I'm trying to not use any stock images, those are no fun to use. I want to make them myself)</p>
+        <p className={"text-xs font-light"}>(I'm trying to not use any stock images, those are no fun to use. I want to
+            make them myself)</p>
     </footer>
 
 }
